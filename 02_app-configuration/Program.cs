c470a2eff07327d57b41774f381bebd32d8e9275
Marcin Borowicz <marcinbor85@gmail.com>
2024-08-30 12:18:31 +0200

    add app configuration basic example

---
+++ 02_app-configuration/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmbeddedDevices.Dotnet.Playground;
+
+public interface IProgram
+{
+        void HelloWorld();
+}
+
+public class Program : IProgram
+{
+        private readonly IConfiguration _config;
+
+        static void Main(string[] args)
+        {
+                // create configuration builder
+                var builder = new ConfigurationBuilder();
+                // load add configuration settings file to builder
+                builder.AddJsonFile("settings.json");
+                // build configuration
+                var config = builder.Build();
+
+                // create service collection
+                var collection = new ServiceCollection();
+                // add configuration interface to service collection
+                collection.AddSingleton<IConfiguration>(config);
+                // add our Program class to service collection as Singleton
+                collection.AddSingleton<IProgram, Program>();
+
+                // build service provider based on service collection
+                var provider = collection.BuildServiceProvider();
+                
+                // get service by its interface
+                var program = provider.GetRequiredService<IProgram>();
+
+                // call method interface
+                program.HelloWorld();
+        }
+
+        public Program(IConfiguration config)
+        {
+                // get confguration interface through dependency injection
+                _config = config;
+        }
+
+        public void HelloWorld()
+        {
+                // get config item from configuration interface
+                var text = _config.GetValue<string>("application:text");
+                // print value
+                Console.WriteLine($"text = {text}");
+        }
+}
